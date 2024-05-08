24af0c5307c2ef21d7a4b749fac0ba91a4b7be9f
Python <srivardhanthota.marolix@gmail.com>
2024-05-08 20:34:56 +0530

    Status Updated

--- Selenium Open Cart python/OpenCart Selenium Python/tests/Test_Register.py
+++ Selenium Open Cart python/OpenCart Selenium Python/tests/Test_Register.py
@@ -17,23 +17,23 @@ class Test_Register:
     password = "Thota@7680"
     email = "srivardhanthota.maroli@gmail.com"
     logger = LogGen.loggen()
-    def test_register_with_mandatory_fiels(self,setup):
-        self.logger.info("******Registering******")
-        self.driver = setup
-        self.driver.get(self.url)
-        self.rg = Register(self.driver)
-        self.rg.home()
-        self.rg.setFirstName(self.firstname)
-        self.rg.setLastName(self.lastname)
-        # self.email = random_email_generator() + "@gmail.com"
-        self.rg.setEmail(self.email)
-        self.rg.setPassword(self.password)
-        time.sleep(3)
-        self.rg.Subscribe()
-        time.sleep(3)
-        self.rg.Privacy()
-        time.sleep(5)
-        self.rg.Continue_btn()
+    # def test_register_with_mandatory_fiels(self,setup):
+    #     self.logger.info("******Registering******")
+    #     self.driver = setup
+    #     self.driver.get(self.url)
+    #     self.rg = Register(self.driver)
+    #     self.rg.home()
+    #     self.rg.setFirstName(self.firstname)
+    #     self.rg.setLastName(self.lastname)
+    #     # self.email = random_email_generator() + "@gmail.com"
+    #     self.rg.setEmail(self.email)
+    #     self.rg.setPassword(self.password)
+    #     time.sleep(3)
+    #     self.rg.Subscribe()
+    #     time.sleep(3)
+    #     self.rg.Privacy()
+    #     time.sleep(5)
+    #     self.rg.Continue_btn()
 
 
 def random_email_generator(self,size=8, chars=string.ascii_lowercase + string.digits):
